0104d8ba563361e31ba0bf10a9687b20a1649cb8
Pen-y-Fan <{ID}+{username}@users.noreply.github.com>
2019-10-15 21:43:05 +0100

    Added PHPUnit and some basic tests

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        $this->assertTrue(true);
+    }
+}
