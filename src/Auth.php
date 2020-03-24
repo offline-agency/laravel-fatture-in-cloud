0b4fd399ec2c7bafd97d908eae90154a664aa9e2
Giacomo Fabbian <giacomo.fabbian@studenti.unipd.it>
2020-03-24 15:07:28 +0100

    [GF] Cast error return from array to object

--- src/Auth.php
+++ src/Auth.php
@@ -58,7 +58,7 @@ class Auth
 
             return self::parseResponse($result);
         } catch (Exception $e) {
-            return [
+            return (object)[
                 'error'   => $e->getMessage(),
                 'code'    => $e->getCode(),
                 'success' => false,
