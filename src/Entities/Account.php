810afc9a24954b6be0aba0d8c5e1f67fb34d724d
Giacomo Fabbian <giacomo.fabbian@studenti.unipd.it>
2021-09-16 23:37:39 +0200

    feat: add additional methods on account

--- src/Entities/Account.php
+++ src/Entities/Account.php
@@ -8,6 +8,28 @@ use OfflineAgency\FattureInCloud\Requests\Account as Request;
 class Account extends FattureInCloud
 {
     /**
+     * @return mixed|string
+     */
+    public function getGenericInfo()
+    {
+        $data = [];
+        Request::info($data);
+
+        return $this->auth->post('richiesta/info', $data);
+    }
+
+    /**
+     * @return mixed|string
+     */
+    public function getLicenseDuration()
+    {
+        $data = [
+            'campi' => 'durata_licenza'
+        ];
+        return $this->getInfo($data);
+    }
+
+    /**
      * @param array $data
      *
      * @return mixed|string
@@ -20,14 +42,79 @@ class Account extends FattureInCloud
     }
 
     /**
-     * @param array $data
-     *
      * @return mixed|string
      */
-    public function getGenericInfo($data = ['nome'])
+    public function getLicenceType()
     {
-        Request::info($data);
+        $data = [
+            'campi' => 'tipo_licenza'
+        ];
+        return $this->getInfo($data);
+    }
 
-        return $this->auth->post('richiesta/info', $data);
+    /**
+     * @return mixed|string
+     */
+    public function getCurrencyList()
+    {
+        $data = [
+            'campi' => 'lista_valute'
+        ];
+        return $this->getInfo($data);
+    }
+
+    /**
+     * @return mixed|string
+     */
+    public function getVatList()
+    {
+        $data = [
+            'campi' => 'lista_iva'
+        ];
+        return $this->getInfo($data);
+    }
+
+    /**
+     * @return mixed|string
+     */
+    public function getCountryList()
+    {
+        $data = [
+            'campi' => 'lista_paesi'
+        ];
+        return $this->getInfo($data);
+    }
+
+    /**
+     * @return mixed|string
+     */
+    public function getTemplateList()
+    {
+        $data = [
+            'campi' => 'lista_template'
+        ];
+        return $this->getInfo($data);
+    }
+
+    /**
+     * @return mixed|string
+     */
+    public function getPaymentMethodForSalesList()
+    {
+        $data = [
+            'campi' => 'lista_conti'
+        ];
+        return $this->getInfo($data);
+    }
+
+    /**
+     * @return mixed|string
+     */
+    public function getPaymentMethodForPurchaseList()
+    {
+        $data = [
+            'campi' => 'lista_metodi_pagamento'
+        ];
+        return $this->getInfo($data);
     }
 }
