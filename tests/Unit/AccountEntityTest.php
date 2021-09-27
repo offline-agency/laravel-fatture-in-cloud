1d1b2142f3fc05c6342e5c82b0a42235ce8594a3
Giacomo Fabbian <giacomo.fabbian@studenti.unipd.it>
2021-09-27 17:49:34 +0200

    feat: add more test to AccountEntity

--- tests/Unit/AccountEntityTest.php
+++ tests/Unit/AccountEntityTest.php
@@ -9,58 +9,88 @@ use Tests\TestCase;
 
 class AccountEntityTest extends TestCase
 {
-    public function test_country_list()
-    {
-        Http::fake([
-            'info/account' => Http::response(
-              (new AccountFakeResponse)->getCountryList()
-            ),
-        ]);
+  public function test_country_list()
+  {
+    Http::fake([
+      'info/account' => Http::response(
+        (new AccountFakeResponse)->getCountryList()
+      ),
+    ]);
 
-        $account = new Account();
-        $response = $account->getCountryList();
+    $account = new Account();
+    $response = $account->getCountryList();
 
-        $this->assertIsArray($response['lista_paesi']); // [] string
+    $this->assertIsArray($response['lista_paesi']); // [] string
     $this->assertIsString($response['lista_paesi'][0]); // Afghanistan
     $this->assertTrue($response['success']); // true
-    }
+  }
 
-    public function test_currency_list()
-    {
-        Http::fake([
-            'info/account' => Http::response(
-              (new AccountFakeResponse)->getCurrencyList()
-            ),
-        ]);
+  public function test_currency_list()
+  {
+    Http::fake([
+      'info/account' => Http::response(
+        (new AccountFakeResponse)->getCurrencyList()
+      ),
+    ]);
 
-        $account = new Account();
-        $response = $account->getCurrencyList();
+    $account = new Account();
+    $response = $account->getCurrencyList();
 
-        $currency = $response['lista_valute'][0];
-        $codice = $currency['codice']; //AED
+    $currency = $response['lista_valute'][0];
+    $codice = $currency['codice']; //AED
     $simbolo = $currency['codice']; //AED
     $cambio = $currency['codice']; //4.32170
 
     $this->assertIsArray($response['lista_valute']);
-        $this->assertIsArray($currency);
-        $this->assertIsString($codice);
-        $this->assertIsString($simbolo);
-        $this->assertIsString($cambio);
-        $this->assertTrue($response['success']); // true
-    }
-
-    public function test_licence_type()
-    {
-        Http::fake([
-            'info/account' => Http::response(
-              (new AccountFakeResponse)->getLicenceType()
-            ),
-        ]);
-
-        $account = new Account();
-        $response = $account->getLicenceType();
-
-        $this->assertIsString($response['tipo_licenza']); // prova
+    $this->assertIsArray($currency);
+    $this->assertIsString($codice);
+    $this->assertIsString($simbolo);
+    $this->assertIsString($cambio);
     $this->assertTrue($response['success']); // true
-    }
+  }
+
+  public function test_licence_type()
+  {
+    Http::fake([
+      'info/account' => Http::response(
+        (new AccountFakeResponse)->getLicenceType()
+      ),
+    ]);
+
+    $account = new Account();
+    $response = $account->getLicenceType();
+
+    $this->assertIsString($response['tipo_licenza']); // prova
+    $this->assertTrue($response['success']); // true
+  }
+
+  public function test_licence_duration()
+  {
+    Http::fake([
+      'info/account' => Http::response(
+        (new AccountFakeResponse)->getLicenseDuration()
+      ),
+    ]);
+
+    $account = new Account();
+    $response = $account->getLicenseDuration();
+
+    $this->assertIsString($response['tipo_licenza']); // prova
+    $this->assertTrue($response['success']); // true
+  }
+
+  public function test_licence_payment_method_for_purchase_list()
+  {
+    Http::fake([
+      'info/account' => Http::response(
+        (new AccountFakeResponse)->getPaymentMethodForPurchaseList()
+      ),
+    ]);
+
+    $account = new Account();
+    $response = $account->getPaymentMethodForPurchaseList();
+
+    $this->assertIsString($response['tipo_licenza']); // prova
+    $this->assertTrue($response['success']); // true
+  }
 }
